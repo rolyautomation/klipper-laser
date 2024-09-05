031a069b563290e33c77035bfd0c5e2c96c017af
jinqiangC <jinqiang@ecomedge.io>
2024-09-05 09:12:44 +0100

    complete  and test   G1  E3

--- klippy/extras/gcode_move.py
+++ klippy/extras/gcode_move.py
@@ -71,7 +71,7 @@ class GCodeMove:
     def _handle_activate_extruder(self):
         self.reset_last_position()
         self.extrude_factor = 1.
-        self.base_position[3] = self.last_position[3]
+        self.base_position[3+3] = self.last_position[3+3]
     def _handle_home_rails_end(self, homing_state, rails):
         self.reset_last_position()
         for axis in homing_state.get_axes():
@@ -158,6 +158,15 @@ class GCodeMove:
                 else:
                     # value relative to base coordinate position
                     self.last_position[3+3] = v + self.base_position[3+3]
+
+                logging.info("\nG1: absolute_coord=%s absolute_extrude=%s"
+                     " base_position=%s last_position=%s "
+                     " speed_factor=%s extrude_factor=%s speed=%s\n",
+                     self.absolute_coord, self.absolute_extrude,
+                     self.base_position[6], self.last_position[6],
+                     self.speed_factor, 
+                     self.extrude_factor, self.speed)
+                     
             if 'F' in params:
                 gcode_speed = float(params['F'])
                 if gcode_speed <= 0.:
@@ -211,9 +220,9 @@ class GCodeMove:
     def cmd_M221(self, gcmd):
         # Set extrude factor override percentage
         new_extrude_factor = gcmd.get_float('S', 100., above=0.) / 100.
-        last_e_pos = self.last_position[3]
-        e_value = (last_e_pos - self.base_position[3]) / self.extrude_factor
-        self.base_position[3] = last_e_pos - e_value * new_extrude_factor
+        last_e_pos = self.last_position[3+3]
+        e_value = (last_e_pos - self.base_position[3+3]) / self.extrude_factor
+        self.base_position[3+3] = last_e_pos - e_value * new_extrude_factor
         self.extrude_factor = new_extrude_factor
     cmd_SET_GCODE_OFFSET_help = "Set a virtual offset to g-code positions"
     def cmd_SET_GCODE_OFFSET(self, gcmd):
@@ -262,12 +271,12 @@ class GCodeMove:
         self.speed_factor = state['speed_factor']
         self.extrude_factor = state['extrude_factor']
         # Restore the relative E position
-        e_diff = self.last_position[3] - state['last_position'][3]
-        self.base_position[3] += e_diff
+        e_diff = self.last_position[3+3] - state['last_position'][3+3]
+        self.base_position[3+3] += e_diff
         # Move the toolhead back if requested
         if gcmd.get_int('MOVE', 0):
             speed = gcmd.get_float('MOVE_SPEED', self.speed, above=0.)
-            self.last_position[:3] = state['last_position'][:3]
+            self.last_position[:6] = state['last_position'][:6]
             self.move_with_transform(self.last_position, speed)
     cmd_GET_POSITION_help = (
         "Return information on the current location of the toolhead")
