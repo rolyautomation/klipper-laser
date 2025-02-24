633aedbaf8002df3f7584330fd845e3331f63e4a
jinqiangC <jinqiang@ecomedge.io>
2025-02-24 12:31:56 +0000

    add  laserstore function

--- klippy/extras/laserstore.py
+++ klippy/extras/laserstore.py
@@ -1,13 +1,12 @@
 # laser store interface
-#
+# file: laserstore.py
 # Copyright (C) 2025-2028  jinqiang <jinqiang@ecomedge.io>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
-import logging
-
-LASER_T_PWM_BLUE = 0
-LASER_T_OPTICAL_RED = 1
-
+#import logging
+import os, logging, ast
+#LASER_T_PWM_BLUE = 0
+#LASER_T_OPTICAL_RED = 1
 '''
 [galvo_parameters fiber1064]
 rotation_angle: -0.0
@@ -41,30 +40,105 @@ c4: 0.0000239867
 c6: 0.0000245435
 c8: 0.0000245894
 
+fiber1064_rotation_angle = 0.0
+fiber1064_pitch_factor = 0.0
+fiber1064_b_scale_plus = -0.0457644
+fiber1064_b_scale_minus = -0.0358127
+fiber1064_c_scale_plus = -0.0275848
+fiber1064_c_scale_minus = -0.0348631
+fiber1064_b2 =  -0.00000998146
+fiber1064_b4 =  -0.00000927960
+fiber1064_b6 = -0.00000844448
+fiber1064_b8 = -0.00000770474
+fiber1064_c2 = 0.0000186495
+fiber1064_c4 = 0.0000183209
+fiber1064_c6 = 0.0000183374
+fiber1064_c8 = 0.0000191004
+
+diode450_rotation_angle: -0.0
+diode450_pitch_factor: 0.0
+diode450_b_scale_plus: 0.0283316
+diode450_b_scale_minus: 0.0403397
+diode450_c_scale_plus: 0.0251046
+diode450_c_scale_minus: 0.0155440
+diode450_b2: -0.0000103735
+diode450_b4: -0.0000100012
+diode450_b6: -0.00000984857
+diode450_b8: -0.00000864826
+diode450_c2: 0.0000255225
+diode450_c4: 0.0000239867
+diode450_c6: 0.0000245435
+diode450_c8: 0.0000245894
+
+#fiber_selected: 1
+#diode_high: 0
+
 '''
+
+
+
+
 
 
 class LaserStoreInterface:
     def __init__(self, config):
         self.printer = config.get_printer()
         #self.name = config.get_name().split(' ')[-1]
+        self.filename = os.path.expanduser(config.get('filename'))
+
+        self.laser_parameters = {
+            'galvo_params_diode': {
+                'rotation_angle': 0.0,
+                'pitch_factor': 0.0,
+                'working_distance': 0.0,
+                'b_scale_plus': -0.0457644,
+                'b_scale_minus': -0.0358127,
+                'c_scale_plus': -0.0275848,
+                'c_scale_minus': -0.0348631,
+                'b2': -0.00000998146,
+                'b4': -0.00000927960,
+                'b6': -0.00000844448,
+                'b8': -0.00000770474,
+                'c2': 0.0000186495,
+                'c4': 0.0000183209,
+                'c6': 0.0000183374,
+                'c8': 0.0000191004,
+            },
+            'galvo_params_fiber': {
+                'rotation_angle': -0.0,
+                'pitch_factor': 0.0,
+                'working_distance': 0.0,                
+                'b_scale_plus': 0.0283316,
+                'b_scale_minus': 0.0403397,
+                'c_scale_plus': 0.0251046,
+                'c_scale_minus': 0.0155440,
+                'b2': -0.0000103735,
+                'b4': -0.0000100012,
+                'b6': -0.00000984857,
+                'b8': -0.00000864826,
+                'c2': 0.0000255225,
+                'c4': 0.0000239867,
+                'c6': 0.0000245435,
+                'c8': 0.0000245894,
+            }
+        }        
+
 
         self.cur_sellaser = 0
         self.opticalfiber_existf = 0
         self.diode_high = 0
-
-
         self.gcode = self.printer.lookup_object('gcode')
 
-        self.gcode.register_command("QUERY_LASER", self.cmd_QUERY_LASER, desc=self.cmd_QUERY_LASER_help)  
-        self.gcode.register_command("TEST_LASER", self.cmd_TEST_LASER)
-        self.gcode.register_command('CHG_LASER_TYPE', self.cmd_CHG_LASER_TYPE,
-                               desc=self.cmd_CHG_LASER_TYPE_help)  
+        #self.gcode.register_command("QUERY_LASER", self.cmd_QUERY_LASER, desc=self.cmd_QUERY_LASER_help)  
+        #self.gcode.register_command("TEST_LASER", self.cmd_TEST_LASER)
+        #self.gcode.register_command('CHG_LASER_TYPE', self.cmd_CHG_LASER_TYPE,
+        #                       desc=self.cmd_CHG_LASER_TYPE_help)  
 
         self.chg_run_cmd = False                                       
-                                              
-        self.printer.register_event_handler("klippy:connect",
-                                            self._handle_connect_laserctrl)     
+        #self.printer.register_event_handler("klippy:connect",
+                                            #self._handle_connect_laserctrl) 
+
+
 
     def _handle_connect_laserctrl(self):
         extruderpwm1 = self.printer.lookup_object('extruderpwm1')
@@ -123,12 +197,7 @@ class LaserStoreInterface:
             self.diode_high = 1 if diode_high_val > 0 else 0
     
     def get_status(self, eventtime=None):
-        self.get_diode_high_status(eventtime)
-        laser_status = {}
-        laser_status['fiber_available'] = self.opticalfiber_existf
-        laser_status['fiber_selected'] = self.cur_sellaser
-        laser_status['diode_high'] = self.diode_high        
-        return dict(laser_status)
+        return dict(self.laser_parameters)
         
         
 def load_config(config):
